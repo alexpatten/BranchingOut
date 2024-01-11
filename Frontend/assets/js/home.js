bed53a3a02a23fb48cf0d2a8309bc26e3d15776c
Alex Patten <103220430+alexpatten@users.noreply.github.com>
2024-01-10 19:40:26 -0500

    Home page, Login iframe and page

---
+++ Frontend/assets/js/home.js
@@ -0,0 +1,7 @@
+function openIframe() {
+    document.getElementById('iframeContainer').style.display = 'block';
+}
+
+function closeIframe() {
+    document.getElementById('iframeContainer').style.display = 'none';
+}
