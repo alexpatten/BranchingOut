db42951f4fe8aeab75e766e5ecf48d3bcfe718b4
Alex Patten <hello@alexpatten.me>
2024-03-07 18:39:36 -0500

    Update SQL Queries

--- SQL/DBandTableCreations.sql
+++ SQL/DBandTableCreations.sql
@@ -4,12 +4,23 @@ CREATE DATABASE BranchingOut;
 -- Use the Database
 USE BranchingOut;
 
+-- Create Roles Table
+CREATE TABLE Roles (
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
+    role_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+-- Insert a role for standard users
+INSERT INTO Roles (role_name) VALUES ('ROLE_USER');
+
 -- Create User Table
 CREATE TABLE User (
     UserID INT AUTO_INCREMENT PRIMARY KEY,
     Username NVARCHAR(50),
     Password NVARCHAR(50),
-    Email NVARCHAR(100)
+    Email NVARCHAR(100),
+    RoleID INT,
+    FOREIGN KEY (RoleID) REFERENCES Roles(role_id)
 );
 
 -- Create Family Table
